3d8519077c4e67a289eb8e86d623663e2da01dee
walindotel-012 <walindotel@gmail.com>
2025-12-04 10:04:10 -0400

    Configurar rama gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
